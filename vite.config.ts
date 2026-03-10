0b087ae0912ceab608f946bdce2676f871f943fb
shuantsu <92118008+shuantsu@users.noreply.github.com>
2026-03-09 22:47:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   root: './src',
   server: {
-    host: '0.0.0.0', // Permite acesso externo
-    port: 5173       // Porta padrão do Vite
+    host: '0.0.0.0', 
+    port: 5173       
   },
   build: {
     outDir: '../dist',
